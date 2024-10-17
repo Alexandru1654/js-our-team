ee314fec0e33a7840f127e96f1e2ab44f1a29602
alexandru233847 <alexutualexino33@gmail.com>
2024-10-17 15:56:57 +0200

    Fixed

--- script.js
+++ script.js
@@ -1,59 +1,57 @@
 const teamMembers = [
   {
-    name: "Marco Bianchi",
-    role: "Designer",
-    email: "marcobianchi@team.com",
-    img: "img/male1.png"
+      name: "Marco Bianchi",
+      role: "Designer",
+      email: "marcobianchi@team.com",
+      img: "img/male1.png"
   },
   {
-    name: "Laura Rossi",
-    role: "Front-end Developer",
-    email: "laurarossi@team.com",
-    img: "img/female1.png"
+      name: "Laura Rossi",
+      role: "Front-end Developer",
+      email: "laurarossi@team.com",
+      img: "img/female1.png"
   },
   {
-    name: "Giorgio Verdi",
-    role: "Back-end Developer",
-    email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+      name: "Giorgio Verdi",
+      role: "Back-end Developer",
+      email: "giorgioverdi@team.com",
+      img: "img/male2.png"
   },
   {
-    name: "Marta Ipsum",
-    role: "SEO Specialist",
-    email: "martarossi@team.com",
-    img: "img/female2.png"
+      name: "Marta Ipsum",
+      role: "SEO Specialist",
+      email: "martarossi@team.com",
+      img: "img/female2.png"
   },
   {
-    name: "Roberto Lorem",
-    role: "SEO Specialist",
-    email: "robertolorem@team.com",
-    img: "img/male3.png"
+      name: "Roberto Lorem",
+      role: "SEO Specialist",
+      email: "robertolorem@team.com",
+      img: "img/male3.png"
   },
   {
-    name: "Daniela Amet",
-    role: "Analyst",
-    email: "danielaamet@team.com",
-    img: "img/female3.png"
+      name: "Daniela Amet",
+      role: "Analyst",
+      email: "danielaamet@team.com",
+      img: "img/female3.png"
   }
 ];
 
-// Container where cards will be added
 const teamContainer = document.getElementById("team-container");
 
-// Function to create team cards
 teamMembers.forEach(member => {
-    const memberCard = `
-    <div class="col-md-4 mb-4">
-        <div class="card h-100">
-        <img src="${member.img}" class="card-img-top" alt="${member.name}">
-        <div class="card-body">
-            <h5 class="card-title">${member.name}</h5>
-            <p class="card-text">${member.role}</p>
-            <a href="mailto:${member.email}" class="btn btn-primary">Contact</a>
-        </div>
-        </div>
-    </div>
-    `;
-    teamContainer.innerHTML += memberCard;
+  const memberDiv = document.createElement("div");
+  memberDiv.classList.add("team-member");
+  
+  memberDiv.innerHTML = `
+      <img src="${member.img}" alt="${member.name}">
+      <div class="member-info">
+          <h3>${member.name}</h3>
+          <p>${member.role}</p>
+          <a href="mailto:${member.email}">${member.email}</a>
+      </div>
+  `;
+  
+  teamContainer.appendChild(memberDiv);
 });
 
